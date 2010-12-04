37d8155d02e819dba9bbdf4f00979cd01d61e703
Daniel Kłobuszewski <daniel.klobuszewski@gmail.com>
2010-12-04 12:24:56 +0000

    + mniejsze błędy zmiennoprzecinkowe w fizyce
    git-svn-id: svn+ssh://gamma/home/samba/koturj/svn/inz@134 e70bc5d4-e669-4a26-a90f-bfde2a246bde

--- src/phx/phx_kernels.cu
+++ src/phx/phx_kernels.cu
@@ -29,7 +29,8 @@ __device__ float3 get_dV( float3 myPos, float3 theirPos, float theirMass )
 {
 	float3 dir = theirPos - myPos;
 	float r2 = dir.x * dir.x + dir.y * dir.y + dir.z * dir.z;
-	return dir * (dt / ( r2 * sqrtf( r2 ) + 1e-1f) );
+	if( r2 < 1 ) r2 = 1;
+	return dir / r2 * (dt / sqrtf( r2 ) );
 }
 
 __global__ void basic_interaction( float3 *positions, float *masses, float3 *velocities, unsigned *cnt )
@@ -41,20 +42,21 @@ __global__ void basic_interaction( float3 *positions, float *masses, float3 *vel
 	{
 		return;
 	}
-	float3 new_pos = positions[ index ];
+	float3 old_pos = positions[ index ];
+	float3 pos_diff = make_float3( 0,0,0 );
 	float3 new_vel = velocities[ index ];
 
 	for( unsigned i = 0; i < count; ++i )
 	{
 		if( i != index )
 		{
-			new_vel += get_dV( positions[ index ], positions[ i ], masses[ i ] );
-			new_pos += new_vel * dt;
+			new_vel += get_dV( old_pos, positions[ i ], masses[ i ] );
+			pos_diff += new_vel * dt;
 		}
 	}
 
 	__syncthreads();
 
-	positions[ index ] = new_pos;
+	positions[ index ] += pos_diff;
 	velocities[ index ] = new_vel;
 }
